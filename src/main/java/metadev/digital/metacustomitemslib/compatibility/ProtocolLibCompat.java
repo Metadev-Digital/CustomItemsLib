3b86d48b3a11eacac2f44d1b4d11a3f116a6ab18
kevin-sherman <kevin@metadevdigital.com>
2025-06-01 19:04:26 -0400

    Support for ProtocolLib v5.3.0+ | Remove Deprecated Server.Recipes and unused Recipe_Updates

--- src/main/java/metadev/digital/metacustomitemslib/compatibility/ProtocolLibCompat.java
+++ src/main/java/metadev/digital/metacustomitemslib/compatibility/ProtocolLibCompat.java
@@ -9,6 +9,7 @@ public class ProtocolLibCompat {
 
 	private static Plugin mPlugin;
 	private static boolean supported = false;
+	private final String latestSupported = "5.3.0";
 
 	// https://www.spigotmc.org/resources/protocollib.1997/
 
@@ -18,11 +19,11 @@ public class ProtocolLibCompat {
 					.sendMessage(Core.PREFIX + "Compatibility with ProtocolLib is disabled in config.yml");
 		} else {
 			mPlugin = Bukkit.getPluginManager().getPlugin(CompatPlugin.ProtocolLib.getName());
-			if (mPlugin.getDescription().getVersion().compareTo("4.4.0") < 0) {
+			if (mPlugin.getDescription().getVersion().compareTo(latestSupported) >= 0) {
 				Bukkit.getServer().getConsoleSender()
 						.sendMessage(Core.PREFIX + ChatColor.RED + "Your current version of ProtocolLib ("
 								+ mPlugin.getDescription().getVersion()
-								+ ") is not supported by BagOfGold, please upgrade to 4.4.0 or newer.");
+								+ ") is not supported by CustomItemsLib. Please upgrade to " + latestSupported + " or newer.");
 			} else {
 				Bukkit.getConsoleSender().sendMessage(Core.PREFIX + "Enabling compatibility with ProtocolLib ("
 						+ mPlugin.getDescription().getVersion() + ").");
