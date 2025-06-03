0923417cf7d09c2835e7239bfd01059f1529d684
kevin-sherman <kevin@metadevdigital.com>
2025-06-03 18:51:22 -0400

    Add equals override for use in FeatureList hashset && Add manipulators for Feature && Summary commenting

--- src/main/java/metadev/digital/metacustomitemslib/compatibility/Feature.java
+++ src/main/java/metadev/digital/metacustomitemslib/compatibility/Feature.java
@@ -1,17 +1,21 @@
 package metadev.digital.metacustomitemslib.compatibility;
 
+import metadev.digital.metacustomitemslib.compatibility.enums.VersionSetIdentifierEnum;
+
+import java.util.Objects;
+
 /***
  * Representation of disparate features in each compatibility so that it's enablement/support can be managed in a uniform manner
  */
 
 public class Feature {
-    private String name;
-    private VersionSet serverVersion;
-    private VersionSet pluginVersion;
-    private boolean supported;
+    private final String name;
+    private final VersionSet serverVersion;
+    private final VersionSet pluginVersion;
+    private final boolean supported;
     private boolean enabled;
 
-    //******************Constructors********************
+    // ****** CONSTRUCTORS ******
 
     /***
      * Sets a new feature with no bounded version sets that is enabled by default
@@ -39,7 +43,7 @@ public class Feature {
     }
 
     /***
-     *
+     * Sets a new feature with a single bounded version set with variable support and enablement
      * @param name - Name of the feature binding
      * @param singlyBoundedVersionSet - The version set to represent the support criteria for this feature
      * @param classification - Whether the version set represents a bound for the server or plugin version
@@ -55,7 +59,7 @@ public class Feature {
     }
 
     /***
-     *
+     * Sets a new feature with a double bounded version set with variable support and enablement
      * @param name - Name of the feature binding
      * @param serverVersion - The earliest semantic version that this feature was supported on
      * @param pluginVersion - The latest semantic version that this feature was supported on
@@ -70,21 +74,76 @@ public class Feature {
         this.enabled = enabled;
     }
 
-    //********************Functions*********************
+    // ****** MANIPULATORS ******
+
+    /***
+     *
+     * @return - Server compatible semantic version bounds as VersionSet
+     */
     public VersionSet getServerVersionSet() {
         return serverVersion;
     }
 
+    /***
+     *
+     * @return - Plugin compatible semantic version bounds as VersionSet
+     */
     public VersionSet getPluginVersionSet() {
         return pluginVersion;
     }
 
+    /***
+     *
+     * @return - Value of enabled flag
+     */
+    public boolean isEnabled(){
+        return enabled;
+    }
+
+    /***
+     *
+     * @return - Value of supported flag
+     */
+    public boolean isSupported(){
+        return supported;
+    }
+
+    /***
+     *
+     * @return - True if feature is both supported and enabled
+     */
     public boolean isActive(){
         return supported && enabled;
     }
 
+    /***
+     *
+     * @return - Name field of feature
+     */
+    public String getName() {
+        return name;
+    }
+
+    /***
+     *
+     * @param flag - The T/F value for the enablement of this feature
+     */
     public void setEnabled(boolean flag) {
         this.enabled = flag;
+    }
+
+    // ****** HELPER FUNCTIONS ******
+
+    /***
+     * Compare provided object to current feature based off of an equal name value
+     * @param compare - Object containing the feature we are comparing to
+     * @return - The T/F value whether the provided object equals the current
+     */
+    @Override
+    public boolean equals(Object compare) {
+        if(this == compare) return true;
+        if( compare == null || this.getClass() != compare.getClass() ) return false;
+        return Objects.equals(this.name, ((Feature) compare).name);
     }
 }
 
