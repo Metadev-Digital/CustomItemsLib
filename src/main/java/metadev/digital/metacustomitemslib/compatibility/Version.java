a7006b4778952ee57138383fd58b3448b2a2171d
kevin-sherman <kevin@metadevdigital.com>
2025-06-03 18:50:09 -0400

    Version && VersionSet summary commenting

--- src/main/java/metadev/digital/metacustomitemslib/compatibility/Version.java
+++ src/main/java/metadev/digital/metacustomitemslib/compatibility/Version.java
@@ -1,14 +1,15 @@
 package metadev.digital.metacustomitemslib.compatibility;
 
 /***
- * 
+ * Version object containing a boolean set value and a String version provided as a semantic version.
+ * Boolean is always true if a value is provided, and false if not. Where false is being used in place of a nulled object
  */
 public class Version {
     private final boolean set;
     private final String semVer;
 
     /***
-     *
+     * Construct an unset version. Used in a VersionSet when a ceiling or floor is not required for the bounds. False set value used in place of a null
      */
     public Version(){
         this.set = false;
@@ -16,8 +17,8 @@ public class Version {
     }
 
     /***
-     *
-     * @param semVer
+     * Construct a set version based upon a provided semantic version as string
+     * @param semVer - String version provided as semantic version
      */
     public Version(String semVer){
         this.set = true;
--- src/main/java/metadev/digital/metacustomitemslib/compatibility/VersionSet.java
+++ src/main/java/metadev/digital/metacustomitemslib/compatibility/VersionSet.java
@@ -1,7 +1,10 @@
 package metadev.digital.metacustomitemslib.compatibility;
 
+import metadev.digital.metacustomitemslib.compatibility.enums.BoundIdentifierEnum;
+
 /***
- *
+ * A pair of two versions to be used as upper and lower bounds for support of a feature.
+ * Boolean is always true if a Version is provided, and false if not. Where false is being used in place of a nulled object
  */
 public class VersionSet {
     private final boolean set;
@@ -9,7 +12,7 @@ public class VersionSet {
     private final Version floor;
 
     /***
-     *
+     * Construct an unset VersionSet. Used in a Feature when a server or plugin version bound is not required. False set value used in place of a null
      */
     public VersionSet(){
         this.set = false;
@@ -18,9 +21,9 @@ public class VersionSet {
     }
 
     /***
-     *
-     * @param semVer
-     * @param classification
+     * Construct a VersionSet containing only a single bound. Used when there is only a ceiling or floor for the bound used in a Feature.
+     * @param semVer - String version provided as a semantic version
+     * @param classification - BoundIdentifierEnum to represent whether the provided bound is to be used as a ceiling or floor.
      */
     public VersionSet(String semVer, BoundIdentifierEnum classification ){
         this.set = true;
@@ -29,9 +32,9 @@ public class VersionSet {
     }
 
     /***
-     *
-     * @param ceiling
-     * @param floor
+     * Construct a VersionSet containing both a ceiling and floor for version support
+     * @param ceiling - Version object containing the String semantic version to be used as the ceiling of version support
+     * @param floor - Version object containing the String semantic version to be used as the floor of version support
      */
     public VersionSet(String floor, String ceiling){
         this.set = true;
@@ -39,14 +42,27 @@ public class VersionSet {
         this.floor = new Version(floor);
     }
 
+    /***
+     *
+     * @return - Boolean set value of the VersionSet. Would always be true if any valid semantic version is provided to this VersionSet.
+     * Use case for false would be in the event that a feature has no version limitations for plugin/server support
+     */
     public boolean isSet() {
         return set;
     }
 
+    /**
+     * Helper function to identify if the VersionSet binding is doubly bound. I.E. there is both a ceiling and a floor
+     * @return - Boolean value representing if the VersionSet is double bound.
+     */
     public boolean isDoubleBound() {
         return ceiling.isSet() && floor.isSet();
     }
 
+    /***
+     * Helper function to identify if the VersionSet binding being evaluated is to be used as a ceiling or floor
+     * @return - BoundIdentifierEnum value representing whether the VersionSet is essentially a ceiling or floor OR null the VersionSet is not set or is double bound
+     */
     public BoundIdentifierEnum getBoundingIdentifier() {
         if (set && !isDoubleBound()) {
             return ceiling.isSet() ? BoundIdentifierEnum.CEILING : BoundIdentifierEnum.FLOOR;
@@ -54,10 +70,18 @@ public class VersionSet {
         return null;
     }
 
+    /***
+     *
+     * @return - Version object for the ceiling of this VersionSet
+     */
     public Version getCeiling() {
         return ceiling;
     }
 
+    /***
+     *
+     * @return - Version object for the floor of this VersionSet
+     */
     public Version getFloor() {
         return floor;
     }
