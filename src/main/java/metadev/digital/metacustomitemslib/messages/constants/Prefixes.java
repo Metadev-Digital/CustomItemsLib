d64797a6b5e24db22d27f500df828ac40debd90e
kevin-sherman <kevin@metadevdigital.com>
2025-06-05 14:54:24 -0400

    Move Prefixes to their own constants file and out of Core

---
+++ src/main/java/metadev/digital/metacustomitemslib/messages/constants/Prefixes.java
@@ -0,0 +1,12 @@
+package metadev.digital.metacustomitemslib.messages.constants;
+
+import org.bukkit.ChatColor;
+
+// TODO MIGRATE PREFIXES HERE
+public class Prefixes {
+    public static final String PLUGIN = "[MetaCustomItemsLib]";
+    public static final String PREFIX = ChatColor.GOLD + PLUGIN + " " + ChatColor.RESET;
+    public static final String PREFIX_DEBUG = ChatColor.GOLD + PLUGIN + "[Debug] " + ChatColor.RESET;
+    public static final String PREFIX_WARNING = ChatColor.GOLD + PLUGIN + "[Warning] " + ChatColor.RED;
+    public static final String PREFIX_ERROR = ChatColor.GOLD + PLUGIN + "[Error] " + ChatColor.RED;
+}
