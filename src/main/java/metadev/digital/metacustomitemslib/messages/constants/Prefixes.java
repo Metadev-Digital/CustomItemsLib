60bb62b6c407155f004f885e453f776cf0bd1e0f
kevin-sherman <kevin@metadevdigital.com>
2025-06-05 15:33:34 -0400

    Prune []'s on Plugin constant to use elsewhere

--- src/main/java/metadev/digital/metacustomitemslib/messages/constants/Prefixes.java
+++ src/main/java/metadev/digital/metacustomitemslib/messages/constants/Prefixes.java
@@ -4,9 +4,9 @@ import org.bukkit.ChatColor;
 
 // TODO MIGRATE PREFIXES HERE
 public class Prefixes {
-    public static final String PLUGIN = "[MetaCustomItemsLib]";
-    public static final String PREFIX = ChatColor.GOLD + PLUGIN + " " + ChatColor.RESET;
-    public static final String PREFIX_DEBUG = ChatColor.GOLD + PLUGIN + "[Debug] " + ChatColor.RESET;
-    public static final String PREFIX_WARNING = ChatColor.GOLD + PLUGIN + "[Warning] " + ChatColor.RED;
-    public static final String PREFIX_ERROR = ChatColor.GOLD + PLUGIN + "[Error] " + ChatColor.RED;
+    public static final String PLUGIN = "MetaCustomItemsLib";
+    public static final String PREFIX = ChatColor.GOLD + "[" + PLUGIN + "] " + ChatColor.RESET;
+    public static final String PREFIX_DEBUG = ChatColor.GOLD + "[" + PLUGIN + "][Debug] " + ChatColor.RESET;
+    public static final String PREFIX_WARNING = ChatColor.GOLD + "[" + PLUGIN + "][Warning] " + ChatColor.RED;
+    public static final String PREFIX_ERROR = ChatColor.GOLD + "[" + PLUGIN + "][Error] " + ChatColor.RED;
 }
