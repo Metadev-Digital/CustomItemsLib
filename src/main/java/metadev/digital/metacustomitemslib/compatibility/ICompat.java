1fc0455386998230a3b02f37175aa932cf9dce03
kevin-sherman <kevin@metadevdigital.com>
2025-06-20 16:50:18 -0400

    Proper handling of a default method getting plugin name and avoid the NPE old handling was causing

--- src/main/java/metadev/digital/metacustomitemslib/compatibility/ICompat.java
+++ src/main/java/metadev/digital/metacustomitemslib/compatibility/ICompat.java
@@ -6,41 +6,35 @@ import metadev.digital.metacustomitemslib.messages.constants.Prefixes;
 import org.bukkit.plugin.Plugin;
 
 public interface ICompat {
-    Plugin compatPlugin = null;
-
     void start() throws SpinupShutdownException;
     void shutdown() throws SpinupShutdownException;
     boolean isEnabled();
     boolean isSupported();
     boolean isActive();
     boolean isLoaded();
+    String getPluginName();
+    String getPluginVersion();
     Plugin getPluginInstance();
 
     /***
      * Standardized server debug messaging for enabling a plugin compatibility
      */
     default void detectedMessage(){
-        if(compatPlugin != null) {
-            MessageHelper.debug(compatPlugin.getName() + " detected. Validating support and attempting to enable plugin compatibility.");
-        }
+        MessageHelper.debug(this.getPluginName() + " detected. Validating support and attempting to enable plugin compatibility.");
     }
 
     /***
      * Standardized server messaging for a successfully enabled plugin compatibility
      */
     default void successfullyLoadedMessage(){
-        if(compatPlugin != null) {
-            MessageHelper.notice(compatPlugin.getName() + " compatibility successfully loaded.");
-        }
+        MessageHelper.notice(this.getPluginName()+ " compatibility successfully loaded.");
     }
 
     /***
      * Standardized server debug messaging for a successfully shutdown plugin compatibility
      */
     default void successfullyShutdownMessage(){
-        if(compatPlugin != null) {
-            MessageHelper.debug(compatPlugin.getName() + " compatibility successfully shutdown.");
-        }
+        MessageHelper.debug(this.getPluginName() + " compatibility successfully shutdown.");
     }
 
     /***
@@ -48,14 +42,12 @@ public interface ICompat {
      * @param unsupportedFeature - The feature in question that is unsupported
      */
     default void unsupportedMessage(Feature unsupportedFeature) {
-        if(compatPlugin != null){
-            String intro = unsupportedFeature.getName().equals("base") ?
-                    "Your current version of " + compatPlugin.getName() + " ( " + compatPlugin.getDescription().getVersion() + " ) "
-                    : "The requested feature ( " + unsupportedFeature.getName() + " ) ";
+        String intro = unsupportedFeature.getName().equals("base") ?
+                "Your current version of " + this.getPluginName() + " ( " + this.getPluginVersion() + " ) "
+                : "The requested feature ( " + unsupportedFeature.getName() + " ) ";
 
-            MessageHelper.error(intro + " is not supported by your current versions of " + Prefixes.PLUGIN + " and Minecraft. " +
-                    "This feature is supported in version(s): " + unsupportedFeature.getConstraintsExplanationText());
-        }
+        MessageHelper.error(intro + " is not supported by your current versions of " + Prefixes.PLUGIN + " and Minecraft. " +
+                "This feature is supported in version(s): " + unsupportedFeature.getConstraintsExplanationText());
     }
 
     /***
@@ -64,13 +56,11 @@ public interface ICompat {
      * @param helpText - String help text on what the enable criteria for this feature is when relevant
      */
     default void disabledMessage(Feature disabledFeature, String helpText) {
-        if(compatPlugin != null){
             String intro = disabledFeature.getName().equals("base") ?
-                    "Compatibility for " + compatPlugin.getName() + " "
-                    : "Compatibility for the feature " + disabledFeature.getName() + " ";
+                "Compatibility for " + this.getPluginName() + " "
+                : "Compatibility for the feature " + disabledFeature.getName() + " ";
 
-            MessageHelper.error(intro + " is currently disabled." + (!helpText.isEmpty() ? " To enable: " + helpText : ""));
-        }
+        MessageHelper.error(intro + " is currently disabled." + (!helpText.isEmpty() ? " To enable: " + helpText : ""));
     }
 
     /***
@@ -78,10 +68,8 @@ public interface ICompat {
      * @param error - String value of any error help text
      */
     default void pluginError(String error){
-        if(compatPlugin != null) {
-            MessageHelper.error(" Compatibility with " + compatPlugin.getName() + " ran into a critical runtime error.");
-            if(!error.isEmpty()) MessageHelper.error(error);
-        }
+        MessageHelper.error(" Compatibility with " + this.getPluginName() + " ran into a critical runtime error.");
+        if(!error.isEmpty()) MessageHelper.error(error);
     }
 }
 
